a3cd3d74ba4899e3819cc6d379fe3685714d7cd7
dibkb <dibas9110@gmail.com>
2024-06-18 23:40:11 +0530

    deployment attempt

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -3,9 +3,8 @@ import Videourlinput from "@/components/Inputfields/Videoinputfield";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useGetBasicInfo } from "@/hooks/useGetBasicInfo";
 import { formatDuration } from "@/utils";
-import { usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import ReactPlayer from "react-player/youtube";
 
 export default function Video() {
@@ -15,64 +14,64 @@ export default function Video() {
     setIsClient(true);
   }, []);
   if (!isClient) return null;
+  // TODO : skeleton loader
   if (loading) return "Loading...";
   return (
-    <main className="container flex flex-col gap-4 max-w-[900px]">
-      <span className="flex justify-center">
-        <Videourlinput buttonText="Go" />
-      </span>
-      <div className="w-full mx-auto shadow-xl rounded-2xl">
-        <div className="relative pt-[56.25%] rounded-2xl">
-          <ReactPlayer
-            className="absolute top-0 left-0 react-player"
-            url={apiResponse?.url}
-            controls={true}
-            width="100%"
-            height="100%"
-            style={{
-              borderRadius: "40px",
-            }}
-          />
+    <Suspense>
+      <main className="container flex flex-col gap-4 max-w-[900px]">
+        <span className="flex justify-center">
+          <Videourlinput buttonText="Go" />
+        </span>
+        <div className="w-full mx-auto shadow-xl rounded-2xl">
+          <div className="relative pt-[56.25%] rounded-2xl">
+            <ReactPlayer
+              className="absolute top-0 left-0 react-player"
+              url={apiResponse?.url}
+              controls={true}
+              width="100%"
+              height="100%"
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-2">
-        <span className="border-b border-stone-300 pb-2">
-          <h1 className="text-xl font-medium ">{apiResponse?.title}</h1>
-          <span className="mt-1 text-sm text-stone-600 flex justify-between items-center">
-            <h2 className="">{apiResponse?.views.text}</h2>
-            <span className="text-xs font-medium flex gap-4">
-              <h3 className="flex items-center gap-1">
-                <span className="size-1 bg-stone-900 rounded-full" />
-                {formatDuration(apiResponse?.duration.lengthSec)}
-              </h3>
-              <h3 className="flex items-center gap-1">
-                <span className="size-1 bg-stone-900 rounded-full" />
-                {apiResponse?.published.pretty}
-              </h3>
+        <div className="flex flex-col gap-2">
+          <span className="border-b border-stone-300 pb-2">
+            <h1 className="text-xl font-medium ">{apiResponse?.title}</h1>
+            <span className="mt-1 text-sm text-stone-600 flex justify-between items-center">
+              <h2 className="">{apiResponse?.views.text}</h2>
+              <span className="text-xs font-medium flex gap-4">
+                <h3 className="flex items-center gap-1">
+                  <span className="size-1 bg-stone-900 rounded-full" />
+                  {formatDuration(apiResponse?.duration.lengthSec)}
+                </h3>
+                <h3 className="flex items-center gap-1">
+                  <span className="size-1 bg-stone-900 rounded-full" />
+                  {apiResponse?.published.pretty}
+                </h3>
+              </span>
             </span>
           </span>
-        </span>
-        <Link
-          className="flex items-center gap-2 w-min px-2 py-1 
+          <Link
+            className="flex items-center gap-2 w-min px-2 py-1 
           rounded-xl hover:bg-stone-300"
-          href={apiResponse?.channel?.url || "#"}
-          target="_blank"
-        >
-          <Avatar>
-            <AvatarImage
-              src={apiResponse?.channel?.icons?.at(0)?.url}
-              alt="@shadcn"
-            />
-            <AvatarFallback>{apiResponse?.channel.name}</AvatarFallback>
-          </Avatar>
-          <h2 className="font-medium whitespace-nowrap">
-            {apiResponse?.channel.name}
-            <p className="text-xs text-stone-600">
-              {apiResponse?.channel.subscribers.pretty}
-            </p>
-          </h2>
-        </Link>
-      </div>
-    </main>
+            href={apiResponse?.channel?.url || "#"}
+            target="_blank"
+          >
+            <Avatar>
+              <AvatarImage
+                src={apiResponse?.channel?.icons?.at(0)?.url}
+                alt="@shadcn"
+              />
+              <AvatarFallback>{apiResponse?.channel.name}</AvatarFallback>
+            </Avatar>
+            <h2 className="font-medium whitespace-nowrap">
+              {apiResponse?.channel.name}
+              <p className="text-xs text-stone-600">
+                {apiResponse?.channel.subscribers.pretty}
+              </p>
+            </h2>
+          </Link>
+        </div>
+      </main>
+    </Suspense>
   );
 }
