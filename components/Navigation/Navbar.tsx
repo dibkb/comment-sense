f32c8a56f1cda3b74789e7abf87a47d5c4c9bedd
dibkb <dibas9110@gmail.com>
2024-06-22 13:56:25 +0530

    changed border navbar

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div
       className={cn(
-        "container py-2 border-b border-stone-300 flex items-center justify-between",
+        "container py-2 border-b flex items-center justify-between",
         heading.className
       )}
     >
