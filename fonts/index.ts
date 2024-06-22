633d576064735eee4249bc66293aa7b5e68a1531
dibkb <dibas9110@gmail.com>
2024-06-22 13:52:25 +0530

    chnaged body font

--- fonts/index.ts
+++ fonts/index.ts
@@ -1,3 +1,3 @@
-import { Playfair_Display, Urbanist, Inter } from "next/font/google";
+import { Playfair_Display, Raleway, Inter } from "next/font/google";
 export const heading = Playfair_Display({ subsets: ["latin"] });
-export const body = Urbanist({ subsets: ["latin"] });
+export const body = Raleway({ subsets: ["latin"] });
