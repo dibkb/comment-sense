12d35150e3741fc605a0d429011be8400dbbccfd
dibkb <dibas9110@gmail.com>
2024-06-26 00:03:56 +0530

    self signed

--- axios/index.ts
+++ axios/index.ts
@@ -3,5 +3,5 @@ export const nodeApiInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_NODE_API,
 });
 export const fastApiInstance = axios.create({
-  baseURL: "https://comments-fastapi.dibkb.xyz",
+  baseURL: "https://commentsapi.dibkb.xyz:8000",
 });
