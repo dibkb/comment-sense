ba7d986830607be2607327399cc66d2da599138e
dibkb <dibas9110@gmail.com>
2024-07-11 03:27:15 +0530

    response

--- components/Videopage/Chat/Chatcomponent.tsx
+++ components/Videopage/Chat/Chatcomponent.tsx
@@ -31,7 +31,7 @@ const Chatcomponent = ({ fullScreen = false, parentRef }: Chatcomponent) => {
       ...prev,
       {
         creator: "ai",
-        message: query,
+        message: data,
       },
     ]);
     setLoading(false);
